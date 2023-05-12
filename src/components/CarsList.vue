eb95840dc51f4f27e223f5cc6b7a66732cb14d14
Daniel Perez <danielperez9403@gmail.com>
2023-05-11 23:23:12 -0400

    Add error handling and update CarList component.vue

--- src/components/CarsList.vue
+++ src/components/CarsList.vue
@@ -1,56 +1,68 @@
 <template>
     <div class="post">
-        <div v-if="loading" class="loading d-flex justify-content-center mt-5 fs-4">
-            <span>Loading...</span>
+        <div v-if="error" class="content">
+            <div class="d-flex justify-content-center mt-5">
+                <h2>ERROR {{ error.status }}</h2>
+            </div>
+            <div class="d-flex justify-content-center mt-2 fs-4">
+                <p v-if="error.status === 404">The requested resource was not found.</p>
+                <p v-else-if="error.status === 500">There was a server error.</p>
+                <p v-else>An unknown error occurred.</p>
+            </div>
         </div>
+        <div v-else>
+            <div v-if="loading" class="loading d-flex justify-content-center mt-5 fs-4">
+                <span>Loading...</span>
+            </div>
 
-        <div v-if="cars" class="content">
-            <div class="row justify-content-center mt-4 mb-4">
-                <div class="row col-10">
-                    <div class="col-lg-3 col-md-4 col-sm-5 col-6 px-0 me-3 mb-md-0 mb-3">
-                        <label for="dealer" class="ms-1 mb-1 fw-bold"> Dealer: </label>
-                        <select id="dealer" class="form-select" v-model="dealer">
-                            <option>Dealer 1</option>
-                            <option>Dealer 2</option>
-                        </select>
-                    </div>
-                    <div class="col-lg-3 col-md-4 col-sm-5 col-6 px-0">
-                        <label for="sorting" class="ms-1 mb-1 fw-bold"> Sort by: </label>
-                        <select id="sorting" class="form-select" v-model="sorting">
-                            <option selected disabled value=""> - Select an option - </option>
-                            <option value="brand">Brand (A - Z)</option>
-                            <option value="model">Model (A - Z)</option>
-                            <option value="price_low_high">Price (Low to High)</option>
-                            <option value="price_high_low">Price (High to Low)</option>
-                        </select>
+            <div v-if="cars" class="content">
+                <div class="row justify-content-center mt-4 mb-4">
+                    <div class="row col-10">
+                        <div class="col-lg-3 col-md-4 col-sm-5 col-6 px-0 me-3 mb-md-0 mb-3">
+                            <label for="dealer" class="ms-1 mb-1 fw-bold"> Dealer: </label>
+                            <select id="dealer" class="form-select" v-model="dealer">
+                                <option>Dealer 1</option>
+                                <option>Dealer 2</option>
+                            </select>
+                        </div>
+                        <div class="col-lg-3 col-md-4 col-sm-5 col-6 px-0">
+                            <label for="sorting" class="ms-1 mb-1 fw-bold"> Sort by: </label>
+                            <select id="sorting" class="form-select" v-model="sorting">
+                                <option selected disabled value=""> - Select an option - </option>
+                                <option value="brand">Brand (A - Z)</option>
+                                <option value="model">Model (A - Z)</option>
+                                <option value="price_low_high">Price (Low to High)</option>
+                                <option value="price_high_low">Price (High to Low)</option>
+                            </select>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="row justify-content-center">
-                <div class="col-10 table-responsive">
-                    <table class="table table-hover">
-                        <thead>
-                            <tr>
-                                <th>Brand</th>
-                                <th>Model</th>
-                                <th>Price</th>
-                                <th>Mileage</th>
-                                <th>Color</th>
-                                <th>Status</th>
-                            </tr>
-                        </thead>
+                <div class="row justify-content-center">
+                    <div class="col-10 table-responsive">
+                        <table class="table table-hover">
+                            <thead>
+                                <tr>
+                                    <th>Brand</th>
+                                    <th>Model</th>
+                                    <th>Price</th>
+                                    <th>Mileage</th>
+                                    <th>Color</th>
+                                    <th>Status</th>
+                                </tr>
+                            </thead>
 
-                        <tbody>
-                            <tr v-for="(car, index) in cars" :key="index">
-                                <td>{{ car.brand }}</td>
-                                <td>{{ car.model }}</td>
-                                <td>{{ car.price }}</td>
-                                <td>{{ car.mileage }}</td>
-                                <td>{{ car.color }}</td>
-                                <td>{{ car.status }}</td>
-                            </tr>
-                        </tbody>
-                    </table>
+                            <tbody>
+                                <tr v-for="(car, index) in cars" :key="index">
+                                    <td>{{ car.Brand }}</td>
+                                    <td>{{ car.Model }}</td>
+                                    <td>{{ car.Price }}</td>
+                                    <td>{{ car.Mileage }}</td>
+                                    <td>{{ car.Color }}</td>
+                                    <td>{{ car.Status }}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
@@ -66,7 +78,8 @@
                 loading: false,
                 cars: null,
                 dealer: "Dealer 1",
-                sorting: ""
+                sorting: "",
+                error: null
             };
         },
         created() {
@@ -81,49 +94,66 @@
             'sorting': 'sortData'
         },
         methods: {
-            fetchData() {
+            async fetchData() {
                 this.cars = null;
                 this.loading = true;
-                fetch(`api/cardealer?dealer=${this.dealer === "Dealer 1" ? "Dealer1" : "Dealer2"}`)
-                    .then(r => r.json())
+                await fetch(`api/cardealer?dealer=${this.dealer === "Dealer 1" ? "Dealer1" : "Dealer2"}`)
+                    .then(r => {
+                        if (!r.ok) {
+                            throw r;
+                        }
+
+                        return r.json();
+                    })
                     .then(json => {
-                        this.cars = json;
+                        console.log(json.cars);
+                        this.cars = json.cars;
                         this.loading = false;
                         return;
+                    })
+                    .catch(error => {
+                        console.log(error);
+                        this.handleError(error);
                     });
             },
             sortData() {
                 if (this.sorting === 'brand') {
                     this.cars.sort((a, b) => {
-                        if (a.brand > b.brand) {
+                        if (a.Brand > b.Brand) {
                             return 1;
-                        } else if (a.brand < b.brand) {
+                        } else if (a.Brand < b.Brand) {
                             return -1;
                         } else {
-                            return a.price - b.price;
+                            return a.Price - b.Price;
                         }
                     });
                 } else if (this.sorting === 'model') {
                     this.cars.sort((a, b) => {
-                        if (a.model > b.model) {
+                        if (a.Model > b.Model) {
                             return 1;
-                        } else if (a.model < b.model) {
+                        } else if (a.Model < b.Model) {
                             return -1;
                         } else {
-                            return a.price - b.price;
+                            return a.Price - b.price;
                         }
                     });
                 } else if (this.sorting === 'price_low_high') {
                     this.cars.sort((a, b) => {
-                        return a.price - b.price;
+                        return a.Price - b.Price;
                     });
                 } else if (this.sorting === 'price_high_low') {
                     this.cars.sort((a, b) => {
-                        return b.price - a.price;
+                        return b.Price - a.Price;
                     });
                 }
-                
-            }
+            },
+            handleError(error) {
+                console.log("Hello");
+                console.log(error);
+                this.error = {
+                    status: error.status,
+                };
+            },
         },
     });
 </script>
